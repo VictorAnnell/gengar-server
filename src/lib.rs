01e46e6a584b666e6ba15cd13c3c4e902eb95611
victor-021 <30280491+victor-021@users.noreply.github.com>
2021-03-30 17:18:15 +0200

    Add initial test skeletons

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_add_two() {
+        assert_eq!(4, add_two(2));
+    }
+}
---
+++ tests/integration_test.rs
@@ -0,0 +1,6 @@
+use gengar;
+
+#[test]
+fn it_adds_two() {
+    assert_eq!(4, gengar::add_two(2));
+}
