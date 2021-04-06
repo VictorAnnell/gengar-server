160caacc7a107a9762d4e444a6f383734c1bfd64
victor-021 <30280491+victor-021@users.noreply.github.com>
2021-04-06 12:28:50 +0200

    Auto add user table when setting up db with docker
    Also add adminer container to docker-compose config for easy
    administration if necessary.

---
+++ docker/mysql/init/setup_table.sql
@@ -0,0 +1,13 @@
+USE gengar_dev;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	certs TEXT NOT NULL
+)
+
+INSERT INTO users (name, certs)
+VALUES
+	('user1', 'cert1'),
+	('user2', 'cert2')
