3ddeabadd6395601c0d0e607e10a2625a8c402bf
victor-021 <30280491+victor-021@users.noreply.github.com>
2021-04-08 14:25:18 +0200

    Change table structure of the auto created db

--- docker/mysql/init/setup_table.sql
+++ docker/mysql/init/setup_table.sql
@@ -1,13 +1,45 @@
 USE gengar_dev;
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
-	name TEXT NOT NULL,
-	certs TEXT NOT NULL
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users
+(
+	UserID SERIAL PRIMARY KEY,
+	UserName CHAR(50) NOT NULL
 );
 
-INSERT INTO users (name, certs)
+DROP TABLE IF EXISTS Vaccines;
+CREATE TABLE Vaccines
+(
+	VaccineID SERIAL PRIMARY KEY,
+	VaccineName CHAR(50) NOT NULL
+);
+
+DROP TABLE IF EXISTS UserVaccine;
+CREATE TABLE UserVaccine
+(
+	UserID BIGINT UNSIGNED NOT NULL,
+	VaccineID BIGINT UNSIGNED NOT NULL,
+	CONSTRAINT PK_UserVaccine PRIMARY KEY
+	(
+			UserID,
+			VaccineID
+	),
+	FOREIGN KEY (UserID) REFERENCES Users (UserID),
+	FOREIGN KEY (VaccineID) REFERENCES Vaccines (VaccineID)
+);
+
+INSERT INTO Users (UserName)
 VALUES
-	('user1', 'cert1'),
-	('user2', 'cert2')
+	('user1'),
+	('user2');
+
+INSERT INTO Vaccines (VaccineName)
+VALUES
+	('cert1'),
+	('cert2');
+
+INSERT INTO UserVaccine (UserID, VaccineID)
+VALUES
+	(1, 1),
+	(1, 2),
+	(2, 2);
