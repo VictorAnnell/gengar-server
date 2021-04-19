c9636f48ad400e25b7ac6f26c3b6c20bf54e6d6b
victor-021 <30280491+victor-021@users.noreply.github.com>
2021-04-19 13:37:17 +0200

    Update default database structure

--- docker/mysql/init/setup_table.sql
+++ docker/mysql/init/setup_table.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS Users;
 CREATE TABLE Users
 (
 	UserID SERIAL PRIMARY KEY,
-	UserName CHAR(50) NOT NULL
+	GoogleUserID CHAR(21) NOT NULL
 );
 
 DROP TABLE IF EXISTS Vaccines;
@@ -25,21 +25,23 @@ CREATE TABLE UserVaccine
 			VaccineID
 	),
 	FOREIGN KEY (UserID) REFERENCES Users (UserID),
-	FOREIGN KEY (VaccineID) REFERENCES Vaccines (VaccineID)
+	FOREIGN KEY (VaccineID) REFERENCES Vaccines (VaccineID),
+	RegisterDate DATE NOT NULL,
+	ExpirationDate DATE NOT NULL
 );
 
-INSERT INTO Users (UserName)
+INSERT INTO Users (GoogleUserID)
 VALUES
-	('user1'),
-	('user2');
+	('234385785823438578589'),
+	('418446744073709551615');
 
 INSERT INTO Vaccines (VaccineName)
 VALUES
 	('cert1'),
 	('cert2');
 
-INSERT INTO UserVaccine (UserID, VaccineID)
+INSERT INTO UserVaccine (UserID, VaccineID, RegisterDate, ExpirationDate)
 VALUES
-	(1, 1),
-	(1, 2),
-	(2, 2);
+	(1, 1, '1988-12-30', '2022-03-30'),
+	(1, 2, '2015-02-19', '2021-06-02'),
+	(2, 2, '2020-12-12', '2122-11-01');
